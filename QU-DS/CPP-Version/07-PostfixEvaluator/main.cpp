7b64daa9346435af1aeebe45686992855fc7b6fc
Ghost <Ghost@qq.com>
2025-12-06 23:12:42 +0800

    修复输出格式

--- QU-DS/CPP-Version/07-PostfixEvaluator/main.cpp
+++ QU-DS/CPP-Version/07-PostfixEvaluator/main.cpp
@@ -7,6 +7,6 @@ int main()
     Evaluator eval;
     eval.getString();
     eval.cal();
-    std::cout << std::format("{}\n", eval.StackTOP());
+    std::cout << std::format("{:.1f}\n", eval.StackTOP());
     return 0;
 }

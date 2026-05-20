8c3c48d311de7763f24e2c0a8013ce9c2756f37c
Ghost <Ghost@qq.com>
2026-05-20 11:35:25 +0800

    实现散列

--- QU-DS/CPP-Version/16-HashTable/include/HashTable.h
+++ QU-DS/CPP-Version/16-HashTable/include/HashTable.h
@@ -5,12 +5,16 @@
 #include <memory>
 #include <new>
 
-
 template <typename T>
 class HashTable
 {
     public:
-        HashTable(std::size_t capacity);
+        // 特殊函数
+        HashTable(std::size_t size);
+    private:
+        // static成员函数
+        static consteval bool is_prime(const std::size_t n);
+        static consteval std::size_t find_next_prime(std::size_t n);
     private:
         std::unique_ptr<Slot<T>[]> m_slot;
         std::size_t m_size;
@@ -18,6 +22,40 @@ class HashTable
 };
 
 template <typename T>
-inline HashTable<T>::HashTable(std::size_t size)
+inline HashTable<T>::HashTable(std::size_t size) : m_size(0), m_remains(0)
 {
+    std::size_t prime = find_next_prime(size);
+    m_slot.reset(new (std::nothrow) Slot<T>[prime]);
+    if (m_slot)
+    {
+        m_size = prime;
+        m_remains = prime;
+    }
+}
+
+template <typename T>
+inline consteval bool HashTable<T>::is_prime(const std::size_t n)
+{
+    if (n <= 1)
+    {
+        return false;
+    }
+    for (std::size_t i = 2; i * i <= n; ++i)
+    {
+        if (n % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+template <typename T>
+inline consteval std::size_t HashTable<T>::find_next_prime(std::size_t n)
+{
+    while (!is_prime(n))
+    {
+        ++n;
+    }
+    return n;
 }
